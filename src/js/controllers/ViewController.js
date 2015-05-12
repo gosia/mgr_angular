6f65943fe0312bc88f262f45b4770c4db1ced130
Gosia Jurkiewicz <gosia.jurkiewicz@gmail.com>
2015-05-12 18:11:05 +0200

    Add compressed view option

---
+++ src/js/controllers/ViewController.js
@@ -0,0 +1,43 @@
+'use strict';
+/* global angular */
+
+angular.module('schedulerApp').controller('ViewController', ['$scope', function ($scope) {
+  var COMPRESSED = 'compressed', EXPANDED = 'expanded';
+
+  var state = EXPANDED;
+
+  function toggleIsOpen() {
+    if ($(window).width() > 767) {
+      console.log(!$('body').hasClass('sidebar-collapse'), $('body').hasClass('sidebar-collapse'));
+      return !$('body').hasClass('sidebar-collapse');
+    }
+    else {
+      return $('body').hasClass('sidebar-open');
+    }
+  }
+
+  var changeState = function() {
+    var newState = state === EXPANDED ? COMPRESSED : EXPANDED;
+
+    if (newState === EXPANDED) {
+      $('.view-compress').removeClass('hidden');
+      $('#view-change-button').removeClass('fa-compress').addClass('fa-expand');
+      if (!toggleIsOpen()) {
+        $('[data-toggle="offcanvas"]').click();
+      }
+      $('.content').removeClass('view-compressed');
+    } else {
+      $('.view-compress').addClass('hidden');
+      $('#view-change-button').removeClass('fa-expand').addClass('fa-compress');
+      if (toggleIsOpen()) {
+        $('[data-toggle="offcanvas"]').click();
+      }
+      $('.content').addClass('view-compressed');
+    }
+
+    state = newState;
+  };
+
+  $scope.changeState = changeState;
+}
+]);
