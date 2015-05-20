b50b253b21dd6f4dcd51afb51d0b73ed9c7eeb85
Gosia Jurkiewicz <gosia.jurkiewicz@gmail.com>
2015-05-20 17:56:37 +0200

    Show errors and warnings on file in frontend

--- src/js/controllers/FileEditorController.js
+++ src/js/controllers/FileEditorController.js
@@ -7,8 +7,25 @@ angular.module('schedulerApp').controller('FileEditorController', ['$scope', '$t
 
     $scope.data = {changed: false};
 
+    var colorEditor = function() {
+      var editor = ace.edit('editor');
+      var Range = ace.require('ace/range').Range;
+
+      _.each(_.keys(editor.getSession().getMarkers()), marker => editor.getSession().removeMarker(marker));
+
+      _.each($scope.file.errors, error => {
+        editor.getSession().addMarker(
+          new Range(error.line - 1, 0, error.line - 1, Infinity), 'ace_highlight-' + error.type, 'fullLine'
+        );
+      });
+    };
+
     var onTextChange = function() {
       $scope.data.changed = true;
+      var editor = ace.edit('editor');
+      $scope.file.setContent(editor.getValue()).then(function() {
+        colorEditor();
+      });
       $scope.$apply();
     };
 
@@ -22,14 +39,16 @@ angular.module('schedulerApp').controller('FileEditorController', ['$scope', '$t
       editor.getSession().setMode('ace/mode/text');
       editor.setValue($scope.file.content);
       editor.gotoLine(1);
+      editor.setShowPrintMargin(false);
       editor.getSession().on('change', onTextChange);
+      colorEditor();
     };
 
     var save = function() {
       var editor = ace.edit('editor');
       var newContent = editor.getValue();
       ApiService.saveFile($scope.fileId, newContent).success(function() {
-        $scope.file.content = newContent;
+        $scope.file.setContent(newContent);
         $scope.data.changed = false;
       });
     };
--- src/js/factories/File.js
+++ src/js/factories/File.js
@@ -1,11 +1,14 @@
 'use strict';
 
-angular.module('schedulerApp').factory('File', ['Perms', function(Perms) {
+angular.module('schedulerApp').factory('File', ['Perms', '$q', function(Perms, $q) {
   function File(id, year, linked, content) {
     this.id = id;
     this.year = year;
     this.linked = linked;
-    this.content = content;
+    this.errors = [];
+    this.linesNum = 0;
+    this.hasErrors = false;
+    this.setContent(content);
 
     this.perms = Perms.init();
   }
@@ -17,6 +20,131 @@ angular.module('schedulerApp').factory('File', ['Perms', function(Perms) {
 
   File.initForList = function(apiData) {
     return new File(apiData.id, apiData.year, apiData.linked, undefined);
+  };
+
+  File.prototype.setContent = function(content) {
+    var file = this;
+    return $q(function(resolve) {
+      file.content = content;
+      if (content !== undefined) {
+        file.linesNum = content.split('\n').length;
+        file.validate().then(function () {
+          resolve();
+        });
+      } else {
+        resolve();
+      }
+    });
+  };
+
+  var error = function(message, line) {
+    return {type: 'err', message: message, line: line + 1};
+  };
+  var warning = function(message, line) {
+    return {type: 'war', message: message, line: line + 1};
+  };
+
+  var validateTeacherLine = function(line, lineNum, teacherMap) {
+    if (line.length !== 7) {
+      return error('Linia zawiera ' + line.length + ' elementów zamiast 7', lineNum);
+    }
+
+    var id = line[1];
+    var pensum = parseInt(line[5]);
+
+    if (teacherMap[id] !== undefined) {
+      return error('Zduplikowany prowadzący (patrz linia ' + teacherMap[id].line + ')', lineNum);
+    }
+    if (isNaN(pensum)) {
+      return error('Pensum musi być liczbą', lineNum);
+    }
+
+    teacherMap[id] = {line: lineNum, pensum: pensum, groups: []};
+  };
+
+  var validateGroupLine = function(line, lineNum, teacherMap, groupMap) {
+    if (line.length !== 7) {
+      return error('Linia zawiera ' + line.length + ' elementów zamiast 7', lineNum);
+    }
+
+    var course = line[2];
+    var group = line[3];
+    var hours = parseInt(line[4]);
+    var teacher = line[5];
+
+    if (teacherMap[teacher] === undefined) {
+      return error('Nieznany prowadzący "' + teacher + '"', lineNum);
+    }
+    if (isNaN(hours)) {
+      return error('Godziny muszą być liczbą', lineNum);
+    }
+
+    if (groupMap[course] === undefined) {
+      groupMap[course] = {};
+    }
+    if (groupMap[course][group] === undefined) {
+      groupMap[course][group] = [];
+    }
+    var data = {teacher: teacher, hours: hours};
+    groupMap[course][group].push(data);
+    teacherMap[teacher].groups.push(data);
+  };
+
+  var validateAll = function(teacherMap) {
+    var result = [];
+    _.each(teacherMap, (value, key) => {
+      var hours = _.reduce(_.map(value.groups, x => x.hours), (memo, num) => memo + num, 0);
+      if (hours * 15 !== value.pensum) {
+        result.push(warning('Pensum dla "' + key + '" się nie zgadza: jest ' + hours * 15 + ' godzin zamiast ' + value.pensum, value.line));
+      }
+    });
+    return result;
+  };
+
+  File.prototype.validate = function() {
+    var file = this;
+
+    return $q(function(resolve) {
+      var validationMessages = [];
+      var lines = file.content.split('\n').map(x => x.split('|'));
+      var teacherMap = {}, err, groupMap = {};
+
+      // handle teachers
+      _.each(lines, (line, lineNum) => {
+        if (line.length === 0) {
+          return;
+        }
+        if (line[0] === 'o') {
+          err = validateTeacherLine(line, lineNum, teacherMap);
+          if (err !== undefined) {
+            validationMessages.push(err);
+          }
+        }
+      });
+
+      // handle groups
+      _.each(lines, (line, lineNum) => {
+        if (line.length === 0) {
+          return;
+        }
+        if (line[0] === '1' || line[0] === '2') {
+          err = validateGroupLine(line, lineNum, teacherMap, groupMap);
+          if (err !== undefined) {
+            validationMessages.push(err);
+          }
+        }
+      });
+
+      // handle summary
+      err = validateAll(teacherMap);
+      if (err !== undefined) {
+        validationMessages = validationMessages.concat(err);
+      }
+
+      file.errors = validationMessages;
+      file.hasErrors = _.some(validationMessages, message => message.type === 'err');
+      resolve();
+    });
   };
 
   return File;
